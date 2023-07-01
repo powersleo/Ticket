7028396f0f17650c3672dfafeef3245dce6e4971
powersleo <powersleo@gmail.com>
2023-06-30 20:32:27 -0700

    Initial Commit

---
+++ Ticket/TicketApp.swift
@@ -0,0 +1,20 @@
+//
+//  TicketApp.swift
+//  Ticket
+//
+//  Created by Leo Powers on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TicketApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
